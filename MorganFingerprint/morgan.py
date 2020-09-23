3066d1e5c2f95c10f6dc4fb9583d4dc660a91d23
dcloudf <murkyrussian@gmail.com>
2020-09-23 11:02:35 +0300

    refactored'n'boosted

--- MorganFingerprint/morgan.py
+++ MorganFingerprint/morgan.py
@@ -1,132 +1,71 @@
-from numba import njit
-from numpy import array, copy, ones, uint64, zeros
+from __future__ import annotations
+from CGRtools.algorithms.morgan import tuple_hash
+from collections import defaultdict, deque
+from math import log2
+from numpy import zeros
 from sklearn.base import BaseEstimator, TransformerMixin
+from typing import Any, Collection, Union, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from CGRtools import MoleculeContainer
 
 
 class MorganFingerprint(TransformerMixin, BaseEstimator):
-    def __init__(self, radius=4, length=1024):
-        self._atoms = {}
-        self._bonds = {}
+    def __init__(self, radius: int = 4, length: int = 1024):
         self._radius = radius
+        self._mask = length - 1
         self._length = length
+        self._log = int(log2(length))
 
-    def transform(self, x):
-        arr = self._fragments(self._bfs(x))
-        tuples = {self.tuple_hash(tpl) for tpl in arr}
+    def transform(self, x: Collection):
+        fingerprints = zeros((len(x), self._length))
 
-        fingerprint = [0] * self._length
-        for one in tuples:
-            fingerprint[one & self._length - 1] = 1
-            fingerprint[one >> 10 & self._length - 1] = 1
+        for idx, mol in enumerate(x):
+            fingerprint = fingerprints[idx]
+            arr = self._fragments(self._bfs(mol), mol)
+            hashes = {tuple_hash(tpl) for tpl in arr}
 
-        return fingerprint
+            for one in hashes:
+                fingerprint[one & self._mask - 1] = 1
+                fingerprint[one >> self._log & self._mask - 1] = 1
 
-    def _bfs(self, molecule):
-        self._atoms = {k: int(v) for k, v in molecule.atoms()}
-        self._bonds = {k: {} for k in self._atoms}
-        for k, v, b in molecule.bonds():
-            self._bonds[k][v] = self._bonds[v][k] = b.order
+        return fingerprints
 
-        arr = []
-        for atom in self._atoms:
-            arr.append([atom])
-            start, end = len(arr) - 1, len(arr)
-            while True:
-                for i in range(start, end):
-                    path, last = arr[i], arr[i][-1]
-                    for_adding = [path + [x] for x in self._bonds[last] if x not in path]
-                    arr.extend(for_adding)
-                if len(arr[-1]) == self._radius:
-                    break
-                start, end = end, len(arr)
+    def _bfs(self, molecule: MoleculeContainer) -> list[list[int]]:
+        atoms = molecule._atoms
+        bonds = molecule._bonds
+
+        arr = [[x] for x in atoms]
+        queue = deque(arr)
+        while queue:
+            now = queue.popleft()
+            if len(now) >= self._radius:
+                continue
+            var = [now + [x] for x in bonds[now[-1]] if x not in now]
+            arr.extend(var)
+            queue.extend(var)
         return arr
 
-    def _fragments(self, arr):
+    def _fragments(self, arr: list[list], molecule: MoleculeContainer) -> set[tuple[Union[int, Any], ...]]:
+        atoms = {x: int(a) for x, a in molecule.atoms()}
+        bonds = molecule._bonds
+        cache = defaultdict(dict)
         out = set()
         for frag in arr:
-            frag_out = [self._atoms[frag[0]]]
-            for first, second in zip(frag, frag[1:]):
-                frag_out.extend([self._bonds[first][second], self._atoms[second]])
-            frag_out = tuple(frag_out)
-            if frag_out < frag_out[::-1]:
-                out.add(frag_out)
+            var = [atoms[frag[0]]]
+            for x, y in zip(frag, frag[1:]):
+                b = cache[x].get(y)
+                if not b:
+                    b = cache[x][y] = cache[y][x] = int(bonds[x][y])
+                var.append(b)
+                var.append(atoms[y])
+            var = tuple(var)
+            rev_var = var[::-1]
+            if var > rev_var:
+                out.add(var)
             else:
-                out.add(frag_out[::-1])
+                out.add(rev_var)
         return out
-
-    def tuple_hash(self, v):
-        """
-        Python 3.8 hash for tuples implemented on python.
-        Working only for nonnested tuples.
-        """
-        acc = 0x27D4EB2F165667C5
-        for el in v:
-            acc += el * 0xC2B2AE3D27D4EB4F
-            acc = (acc << 31) | (acc >> 33)
-            acc *= 0x9E3779B185EBCA87
-
-        acc += len(v) ^ 2870177450013471926  # 0xC2B2AE3D27D4EB4F ^ 3527539
-
-        if acc == 0xFFFFFFFFFFFFFFFF:
-            return 1546275796
-        return acc
-
-
-def mol_to_hard_adj(molecule):
-    atoms = array([int(v) for k, v in molecule.atoms()], dtype=uint64)
-    bonds = {x: [] for x, _ in molecule.atoms()}
-    for k, v, b in molecule.bonds():
-        bonds[k].append(v)
-        bonds[v].append(k)
-    max_len = len(max(bonds.values(), key=len))
-    matrix = -ones((len(atoms), max_len))
-    for k in bonds:
-        matrix[k - 1][:len(bonds[k])] = array(bonds[k], dtype=uint64)
-    return atoms, matrix
-
-
-@njit
-def bfs_numba(atoms, adj, length):
-    max_frags = len(atoms) * 4 * 3**(length - 1)
-    arr = zeros((max_frags, length), dtype=uint64)
-    index = 0
-    for i in range(1, len(atoms) + 1):
-        arr[index][0] = i
-        start, end = index, index + 1
-        while True:
-            for j in range(start, end):
-                path = copy(arr[j])
-                where_flag = False
-                for number in path:
-                    if number == 0:
-                        where_flag = True
-                if where_flag:
-                    index += 1
-                    break
-                border = 0
-                for idx, number in enumerate(path):
-                    if number != 0:
-                        border = idx
-                        break
-                last = int(path[border])
-                for k in adj[last - 1]:
-                    where_flag = False
-                    for number in path:
-                        if number == 0:
-                            where_flag = True
-                    if k != -1 and not where_flag:
-                        path[border + 1] = k
-                        index += 1
-                        arr[index] = path
-            where_flag = False
-            for number in path:
-                if number == 0:
-                    where_flag = True
-            if where_flag:
-                index += 1
-                break
-            start, end = end, index + 1
-    return arr
 
 
 __all__ = ['MorganFingerprint']
--- test/test.py
+++ test/test.py
@@ -1,26 +1,17 @@
 from CGRtools.files import SDFRead
+from CIMtools.preprocessing import FragmentorFingerprint
 from MorganFingerprint import MorganFingerprint
-from pickle import load, dump
 from time import time
 
 with SDFRead('2500.sdf') as f:
     mols = f.read()
 
 morgan = MorganFingerprint()
-with open('fitted_fragmentor.pickle', 'rb') as f:
-    fragmentor = load(f)
+fragmentor = FragmentorFingerprint(fingerprint_size=10, min_length=1, max_length=4, doallways=True)
 
-mine, not_mine = [], []
-mine_all, not_mine_all = {}, {}
-for i, mol in enumerate(mols, start=1):
-    now = time()
-    finger = morgan.transform(mol)
-    mine_all[i] = finger
-    mine.append(time() - now)
-    now = time()
-    frag = fragmentor.transform([mol]).values
-    not_mine_all[i] = frag
-    not_mine.append(time() - now)
-    print(f'{i} targets is done')
-print(f'My fingerprints time is {sum(mine)}, their fingerprints time is {sum(not_mine)},'
-      f'my average time if {sum(mine)/len(mine)}, their average time is{sum(not_mine)/len(not_mine)}')
+now = time()
+fingers = morgan.transform(mols)
+print(f'My time is {time() - now}')
+now = time()
+fingers = fragmentor.transform(mols)
+print(f'Their time is {time() - now}')
